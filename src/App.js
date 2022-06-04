438ab8aa989f381804e87d5dfde1b53089d72b24
Raj-Stark <rpal778866@gmail.com>
2022-06-04 15:15:17 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+  return (
+    <main>
+      <section className="menu  section">
+        <div className="title">
+          <h2>Our Menu </h2>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
